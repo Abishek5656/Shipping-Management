a087fc38958cd51298bba403eb2fc3392a3b5452
abishekvarmapenmetsa <abishekvarma@newowl.co>
2025-09-16 16:55:03 +0530

    final push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,9 +13,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
-  // const token = localStorage.getItem("token");
-
-  const token = true;
+  const token = localStorage.getItem("token");
   return (
     <BrowserRouter>
       <Suspense fallback={<div className="text-center mt-5">Loading...</div>}>
